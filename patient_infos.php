3e89ae762c3fdbc4169bd7da620d7729b60c0771
Pedro <pedrofilipe_md@hotmail.com>
2018-11-28 02:14:05 +0000

    faltaram cenas

--- patient_infos.php
+++ patient_infos.php
@@ -4,21 +4,15 @@
 
 
     $patient_id=$_GET['patient_id'];   
-
+    $patient=getPatient($patient_id);
+     
     $path_to_style="main/style.css";
     $path_to_layout="main/layout.css";
     include("templates/header.php");
     
     
 ?>
-name   
-  birthday 
-  cc 
-  weight 
-  height 
-  email 
-  medic_id 
-  notes 
+    <?php /* name birthday cc weight height email medic_id notes */?>
     <table border="1">
         <tr>
             <th>Name</th>
@@ -29,68 +23,16 @@ name
             <th>Email</th>
             <th>Notes</th>
         </tr>
-        <tr>
-        <?php 
-            foreach 
+        <tr>              
+        <th><?=$patient['name']?></th>
+        <th><?=$patient['birthday']?></th>
+        <th><?=$patient['cc']?></th>
+        <th><?=$patient['weight']?></th>
+        <th><?=$patient['height']?></th>
+        <th><?=$patient['email']?></th>
+        <th><?=$patient['notes']?></th>        
+        </tr>
         
-        <th>Q1</th>
-        <th>Q2</th>
-        <th>Q3</th>
-        <th rowspan="2">Grade</th>
-        <th>Q1</th>
-        <th>Q2</th>
-        <th>Q3</th>
-        <th rowspan="2">Grade</th>
-        <tr>
-        <th>Code</th>
-        <th>Name</th>
-        <td>8</td>
-        <td>7</td>
-        <td>5</td>
-        <td>6</td>
-        <td>7</td>
-        <td>7</td>
-        <th>s/ A.</th>
-        <th>c/ A.</th>
-        </tr>
-        <tr>
-        <td>80549061</td>
-        <td>John</td>
-        <td>70%</td>
-        <td>100%</td>
-        <td>100%</td>
-        <td>17.6</td>
-        <td colspan="4">&nbsp;</td>
-        <td>17.6</td>
-        <td>18</td>
-        </tr>
-        <tr>
-        <td>80549062</td>
-        <td>Mary</td>
-        <td>10%</td>
-        <td>50%</td>
-        <td>50%</td>
-        <td>6.8</td>
-        <td>100%</td>
-        <td>100%</td>
-        <td>50%</td>
-        <td>16.5</td>
-        <td>16.5</td>
-        <td>17</td>
-        </tr>
-        <tr>
-        <td>80549063</td>
-        <td>Claire</td>
-        <td colspan="4">&nbsp;</td>
-        <td>50%</td>
-        <td>50%</td>
-        <td>50%</td>
-        <td>10.0</td>
-        <td>10.0</td>
-        <td>10</td>
-        </tr>
-    </table>
-    <img src="http://www.w3c.it/events/2013/css3html5/img/html5-logo.png"
-            alt="Valid HTML5 Icon">
+    </table>    
   </body>
 </html>
